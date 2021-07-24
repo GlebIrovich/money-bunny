1eeafafd36a2a22e205af9f52e9f764c7d3a259b
Gleb Irovich <gleb.irovich@locatee.ch>
2021-07-24 18:07:45 +0200

    feat(money-bunny): wrap application in the state context provider

--- apps/money-bunny/src/app/app.tsx
+++ apps/money-bunny/src/app/app.tsx
@@ -1,17 +1,44 @@
-import { useEffect, useState } from 'react';
-import { ExpenseDto } from '@money-bunny/models';
+import { useEffect } from 'react';
+import { Currency } from '@money-bunny/models';
 import { expensesApiService } from './services/expenses-api.service';
+import {
+  useApplicationDispatch,
+  useApplicationState,
+} from './state/application-state.context';
 
 export function App() {
-  const [items, setItems] = useState<ExpenseDto[]>([]);
+  const state = useApplicationState();
+  const dispatch = useApplicationDispatch();
   useEffect(() => {
-    expensesApiService.getList().then(({ data }) => data && setItems(data));
-  }, []);
+    expensesApiService.getList().then(({ data }) => {
+      if (data) {
+        console.log('Request');
+        dispatch({
+          type: 'ADD_EXPENSES',
+          expenses: data,
+        });
+      }
+    });
+  }, [dispatch]);
+
+  const addItem = () =>
+    dispatch({
+      type: 'ADD_EXPENSE',
+      expense: {
+        id: (Math.random() * 1000).toString(),
+        amount: 100,
+        currency: Currency.CHF,
+        category: 'any',
+        createdAt: new Date().toISOString(),
+        updatedAt: new Date().toISOString(),
+      },
+    });
 
   return (
     <div>
-      {items.map((item) => (
-        <span key={item.id}>{item.amount}</span>
+      <button onClick={addItem}>Click</button>
+      {state.expenses.map((item) => (
+        <div key={item.id}>{item.amount}</div>
       ))}
     </div>
   );
