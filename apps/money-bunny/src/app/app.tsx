6faf91bb44bcfba24d29d053c114cbd2567dabd0
Gleb Irovich <gleb.irovich@locatee.ch>
2021-07-29 14:59:08 +0200

    chore: remove some dead code

--- apps/money-bunny/src/app/app.tsx
+++ apps/money-bunny/src/app/app.tsx
@@ -1,25 +1,9 @@
-import { useEffect } from 'react';
-import { expensesApiService } from './services/expenses-api.service';
-import { useApplicationDispatch } from './state/application-state.context';
 import AppLayoutComponent from './components/app-layout/app-layout.component';
 import { BrowserRouter, Redirect, Route, Switch } from 'react-router-dom';
 import ExpensesPage from './pages/expenses.page';
 import StatisticsPage from './pages/statistics.page';
 
 export function App() {
-  const dispatch = useApplicationDispatch();
-  useEffect(() => {
-    expensesApiService.getList().then(({ data }) => {
-      if (data) {
-        console.log('Request');
-        dispatch({
-          type: 'ADD_EXPENSES',
-          expenses: data,
-        });
-      }
-    });
-  }, [dispatch]);
-
   return (
     <BrowserRouter>
       <AppLayoutComponent>
